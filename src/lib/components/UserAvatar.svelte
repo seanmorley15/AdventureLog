7decfd61e8f14e49adf3d874935cb525da17da9d
Sean Morley <zipsm15@gmail.com>
2024-04-11 13:46:41 +0000

    Add user icon field and update version number

--- src/lib/components/UserAvatar.svelte
+++ src/lib/components/UserAvatar.svelte
@@ -2,7 +2,14 @@
   import { enhance } from "$app/forms";
   import { goto } from "$app/navigation";
   export let user: any;
-  let firstLetter = user.first_name.charAt(0);
+
+  let icon: string = "";
+
+  if (user.icon != null && user.icon != "") {
+    icon = user.icon;
+  } else {
+    icon = user.username.charAt(0);
+  }
 
   async function navToSettings() {
     goto("/settings");
@@ -15,7 +22,7 @@
 <div class="dropdown dropdown-bottom dropdown-end" tabindex="0" role="button">
   <div class="avatar placeholder">
     <div class="bg-neutral text-neutral-content rounded-full w-10 ml-4">
-      <span class="text-2xl -mt-0.5">{firstLetter}</span>
+      <span class="text-2xl -mt-0.5">{icon}</span>
     </div>
   </div>
   <!-- svelte-ignore a11y-missing-attribute -->
@@ -26,6 +33,7 @@
   >
     <!-- svelte-ignore a11y-missing-attribute -->
     <!-- svelte-ignore a11y-missing-attribute -->
+    <p class="text-lg ml-4 font-bold">Hi, {user.first_name} {user.last_name}</p>
     <li><a>Profile</a></li>
     <li><button on:click={navToLog}>My Log</button></li>
     <li><button on:click={navToSettings}>Settings</button></li>
