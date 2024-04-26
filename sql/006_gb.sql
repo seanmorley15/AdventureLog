db8fc061f275fc7ee0f58681e49efe96b41a6171
Sean Morley <zipsm15@gmail.com>
2024-04-26 22:43:27 +0000

    Refactor database schema and remove unused SQL files

--- sql/006_gb.sql
+++
@@ -1,8 +0,0 @@
--- INSERT INTO "worldTravelCountryRegions" (id, name, country_code)
--- VALUES
---   ('GB-ENG', 'England', 'gb'),
---   ('GB-NIR', 'Northern Ireland', 'gb'),
---   ('GB-SCT', 'Scotland', 'gb'),
---   ('GB-WLS', 'Wales', 'gb')
-
--- ON CONFLICT (id) DO NOTHING;
