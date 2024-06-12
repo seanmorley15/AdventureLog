8eb9f11708ba5418a56aa504c891f9d1f1fa6d74
Sean Morley <zipsm15@gmail.com>
2024-06-12 19:49:55 +0000

    chore: Fix recursive call in getBackgroundImages when encountering empty folder placeholder

--- src/lib/db/getBackgroundImages.ts
+++ src/lib/db/getBackgroundImages.ts
@@ -1,7 +1,7 @@
 import { ensureBucketExists, getObjectUrl, s3Client } from "$lib/server/s3";
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 
-export const getBackgroundImages = async () => {
+export const getBackgroundImages = async (): Promise<string> => {
   await ensureBucketExists("backgrounds");
 
   const data = await s3Client.send(
@@ -12,6 +12,10 @@ export const getBackgroundImages = async () => {
   const randomIndex = Math.floor(Math.random() * randomImages.length);
 
   const randomImage = randomImages[randomIndex];
+
+  if (randomImage == ".emptyFolderPlaceholder") {
+    return getBackgroundImages();
+  }
 
   console.log(randomImage);
 
