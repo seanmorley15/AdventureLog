2f5bdc052d546173401be532294b42c8b63a307d
Sean Morley <zipsm15@gmail.com>
2024-06-12 17:59:24 +0000

    chore: Update S3 endpoint configuration to use MINIO_CLIENT_OVERRIDE

--- src/lib/server/s3.ts
+++ src/lib/server/s3.ts
@@ -116,7 +116,12 @@ export const deleteObject = async (bucketName: string, fileName: string) => {
 
 export const getObjectUrl = (bucketName: string, fileName: string): string => {
   let objectUrl: string;
-  let endpoint = env.MINIO_CLIENT_OVERRIDE as string;
+  let endpoint: string = "";
+  if (env.MINIO_CLIENT_OVERRIDE) {
+    endpoint = env.MINIO_CLIENT_OVERRIDE as string;
+  } else {
+    endpoint = env.AWS_S3_ENDPOINT as string;
+  }
 
   if (endpoint.includes("amazonaws.com")) {
     // Amazon S3
