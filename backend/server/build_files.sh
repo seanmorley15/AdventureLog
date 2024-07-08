86824b24f0e76360384e33b8096dd554e5d04d16
Sean Morley <zipsm15@gmail.com>
2024-07-08 11:58:21 -0400

    vercel

---
+++ backend/server/build_files.sh
@@ -0,0 +1,4 @@
+# build_files.sh
+
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
--- backend/server/demo/settings.py
+++ backend/server/demo/settings.py
@@ -122,6 +122,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+
 # TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 TEMPLATES = [
