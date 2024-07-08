56160120878c89efb510440adcdd310b88731a97
Sean Morley <zipsm15@gmail.com>
2024-07-08 11:59:16 -0400

    chore: Add vercel variable to wsgi.py

--- backend/server/demo/wsgi.py
+++ backend/server/demo/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
 
 application = get_wsgi_application()
+# add this vercel variable
+app = application
