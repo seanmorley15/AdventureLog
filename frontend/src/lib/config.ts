94159d3624c590a7ce3b89c64988b9ae10d64637
Sean Morley <98704938+seanmorley15@users.noreply.github.com>
2024-07-09 10:25:34 -0400

    Update config.ts

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-export let appVersion = 'Web v0.3.0';
-export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.3.0';
+export let appVersion = 'Web v0.4.0';
+export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.4.0';
 export let appTitle = 'AdventureLog';
 export let copyrightYear = '2024';
