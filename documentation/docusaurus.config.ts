d64abf22730a364df8315cc0534b0aba09e1232f
Sean Morley <zipsm15@gmail.com>
2024-07-09 13:53:03 -0400

    chore: Update documentation configuration and remove unused files

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs.adventurelog.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
