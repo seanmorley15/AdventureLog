680a46e798da31b0977bf414bf25e77fd1e25aa9
Sean Morley <zipsm15@gmail.com>
2024-07-11 20:59:55 -0400

    generation url

--- backend/server/adventures/views.py
+++ backend/server/adventures/views.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from django.db.models.functions import Lower
@@ -134,3 +135,36 @@ class StatsViewSet(viewsets.ViewSet):
             'country_count': country_count,
             'total_countries': total_countries
         })
+    
+class GenerateDescription(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'],)
+    def desc(self, request):
+        name = self.request.query_params.get('name', '')
+        # un url encode the name
+        name = name.replace('%20', ' ')
+        print(name)
+        url = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=extracts&exintro&explaintext&format=json&titles=%s' % name
+        response = requests.get(url)
+        data = response.json()
+        data = response.json()
+        page_id = next(iter(data["query"]["pages"]))
+        extract = data["query"]["pages"][page_id]
+        if extract.get('extract') is None:
+            return Response({"error": "No description found"}, status=400)
+        return Response(extract)
+    @action(detail=False, methods=['get'],)
+    def img(self, request):
+        name = self.request.query_params.get('name', '')
+        # un url encode the name
+        name = name.replace('%20', ' ')
+        url = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=pageimages&format=json&piprop=original&titles=%s' % name
+        response = requests.get(url)
+        data = response.json()
+        page_id = next(iter(data["query"]["pages"]))
+        extract = data["query"]["pages"][page_id]
+        if extract.get('original') is None:
+            return Response({"error": "No image found"}, status=400)
+        return Response(extract["original"])
+        
