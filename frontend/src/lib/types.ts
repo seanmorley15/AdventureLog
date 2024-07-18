040d5a755f151911936ff2ee1a94b6a424d980b9
Sean Morley <zipsm15@gmail.com>
2024-07-18 15:22:51 -0400

    geocoding!!

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -62,3 +62,20 @@ export type Collection = {
 	is_public: boolean;
 	adventures: Adventure[];
 };
+
+export type OpenStreetMapPlace = {
+	place_id: number;
+	licence: string;
+	osm_type: string;
+	osm_id: number;
+	lat: string;
+	lon: string;
+	category: string;
+	type: string;
+	place_rank: number;
+	importance: number;
+	addresstype: string;
+	name: string;
+	display_name: string;
+	boundingbox: string[];
+};
