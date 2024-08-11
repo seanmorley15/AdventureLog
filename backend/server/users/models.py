b88cd5df8d85c21d3d8a57c5857b7d72cd621fc4
Sean Morley <zipsm15@gmail.com>
2024-08-11 09:07:57 -0400

    users

--- backend/server/users/models.py
+++ backend/server/users/models.py
@@ -5,6 +5,7 @@ from django_resized import ResizedImageField
 
 class CustomUser(AbstractUser):
     profile_pic = ResizedImageField(force_format="WEBP", quality=75, null=True, blank=True, upload_to='profile-pics/')
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     
     def __str__(self):
         return self.username
