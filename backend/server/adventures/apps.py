02d2d62f477644d5917fd747147e7bae20412397
Sean Morley <zipsm15@gmail.com>
2024-08-21 10:26:17 -0400

    remove scheduler for now

--- backend/server/adventures/apps.py
+++ backend/server/adventures/apps.py
@@ -5,7 +5,7 @@ class AdventuresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'adventures'
 
-    def ready(self):
-        if settings.SCHEDULER_AUTOSTART:
-            from .scheduler import start_scheduler
-            start_scheduler()
+    # def ready(self):
+    #     if settings.SCHEDULER_AUTOSTART:
+    #         from .scheduler import start_scheduler
+    #         start_scheduler()
