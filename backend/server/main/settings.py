6eff9f645683201a562f389c02bba6011a0b0e4a
Sean Morley <zipsm15@gmail.com>
2024-11-05 10:15:38 -0500

    Update STATIC_ROOT path to use 'staticfiles' directory

--- backend/server/main/settings.py
+++ backend/server/main/settings.py
@@ -132,13 +132,12 @@ USE_TZ = True
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 
 # TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
