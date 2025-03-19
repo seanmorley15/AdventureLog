c402dff9a7e105dcba4f37e8d7bd17ce70038599
Sean Morley <zipsm15@gmail.com>
2025-03-19 09:29:35 -0400

    chore: Bump app version to v0.9.0 and update changelog link

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-export let appVersion = 'v0.8.0';
-export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.8.0';
+export let appVersion = 'v0.9.0';
+export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.9.0';
 export let appTitle = 'AdventureLog';
 export let copyrightYear = '2023-2025';
