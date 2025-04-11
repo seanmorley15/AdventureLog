seanmorley15/AdventureLog

commit 937c3c6a68e41356fd3de2aba826563951be1f91

Update backend/server/adventures/views/collection_view.py
Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

backend/server/adventures/views/collection_view.py
@@ -22,8 +22,7 @@ class CollectionViewSet(viewsets.ModelViewSet):
         order_by = self.request.query_params.get('order_by', 'name')
         order_direction = self.request.query_params.get('order_direction', 'asc')
 
-        valid_order_by = ['name', 'upated_at', 'start_date']
-        if order_by not in valid_order_by:
+        valid_order_by = ['name', 'updated_at', 'start_date']
             order_by = 'updated_at'
 
         if order_direction not in ['asc', 'desc']:

commit 23426012af3e0987f33b64c07f0cdae419575e52

Fix Python Syntax Error
Commit 937c3c6a68e41356fd3de2aba826563951be1f91 introduced a Python
syntax error, breaking the server. This fixes the issue by restoring the
probably accidental removal of one line of code.

backend/server/adventures/views/collection_view.py
@@ -23,6 +23,7 @@ class CollectionViewSet(viewsets.ModelViewSet):
         order_direction = self.request.query_params.get('order_direction', 'asc')
 
         valid_order_by = ['name', 'updated_at', 'start_date']
+        if order_by not in valid_order_by:
             order_by = 'updated_at'
 
         if order_direction not in ['asc', 'desc']: