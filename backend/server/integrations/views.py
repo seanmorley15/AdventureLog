36a2e59d52cdfb9bd1eab059277987dc39bfc882
Sean Morley <mail@seanmorley.com>
2025-06-05 15:12:40 -0400

    fix: add logging import to integrations views

--- backend/server/integrations/views.py
+++ backend/server/integrations/views.py
@@ -11,6 +11,7 @@ from django.conf import settings
 from adventures.models import AdventureImage
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+import logging
 
 logger = logging.getLogger(__name__)
 
