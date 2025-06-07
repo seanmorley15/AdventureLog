20cdc2405f7291385f3cd77cec69d9fd2b11bf3e
Sean Morley <mail@seanmorley.com>
2025-06-07 10:58:02 -0400

    feat: add alert message in AdventureModal for GPX file tips

--- frontend/src/lib/components/AdventureModal.svelte
+++ frontend/src/lib/components/AdventureModal.svelte
@@ -742,6 +742,23 @@
 							</button>
 						</div>
 
+						<div role="alert" class="alert bg-neutral">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								class="stroke-info h-6 w-6 shrink-0"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+								></path>
+							</svg>
+							<span>{$t('adventures.gpx_tip')}</span>
+						</div>
+
 						{#if attachmentToEdit}
 							<form
 								on:submit={(e) => {
