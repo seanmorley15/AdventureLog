08cd3912c753e85f4d37c7e5bc232c63e7653319
Sean Morley <mail@seanmorley.com>
2025-06-18 19:24:51 -0400

    fix(config): correct appVersion string formatting

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-export let appVersion = 'v0.10.0-main-06152025';
+export let appVersion = 'v0.10.0-main-06182025';
 export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.10.0';
 export let appTitle = 'AdventureLog';
 export let copyrightYear = '2023-2025';
