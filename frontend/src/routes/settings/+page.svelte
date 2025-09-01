0c33fd004a688a6f476033f526689df892f6b35a
Sean Morley <mail@seanmorley.com>
2025-09-01 09:43:40 -0400

    Refactor help documentation link in settings page
    - Updated the condition to display the help documentation link based on the `wandererEnabled` flag.
    - Removed the conditional rendering for staff users and Strava integration status.
    - Changed the documentation link to point to the Immich integration documentation.

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1226,22 +1226,17 @@
 								{/if}
 
 								<!-- Help documentation link -->
-								{#if user.is_staff || !stravaGlobalEnabled}
+
+								{#if !wandererEnabled}
 									<div class="mt-4 p-4 bg-info/10 rounded-lg">
-										{#if user.is_staff}
-											<p class="text-sm">
-												📖 {$t('immich.need_help')}
-												<a
-													class="link link-primary"
-													href="https://adventurelog.app/docs/configuration/wanderer_integration.html"
-													target="_blank">{$t('navbar.documentation')}</a
-												>
-											</p>
-										{:else if !stravaGlobalEnabled}
-											<p class="text-sm">
-												ℹ️ {$t('google_maps.google_maps_integration_desc_no_staff')}
-											</p>
-										{/if}
+										<p class="text-sm">
+											📖 {$t('immich.need_help')}
+											<a
+												class="link link-primary"
+												href="https://adventurelog.app/docs/configuration/immich_integration.html"
+												target="_blank">{$t('navbar.documentation')}</a
+											>
+										</p>
 									</div>
 								{/if}
 							</div>
