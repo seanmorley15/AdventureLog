5f3615f11bba60e9418b4cf496114cc8aba88157
Sean Morley <mail@seanmorley.com>
2026-01-14 10:06:50 -0500

    fix: Correct appVersion to match the latest pre-release version

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-export let appVersion = 'v0.12.0-pre-dev-011026';
+export let appVersion = 'v0.12.0-pre-main-011426';
 export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.11.0';
 export let appTitle = 'AdventureLog';
 export let copyrightYear = '2023-2026';
