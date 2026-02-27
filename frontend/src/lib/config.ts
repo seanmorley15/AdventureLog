054a5b54b2a3eaed76a374662faf050a6ad22b35
Sean Morley <git@seanmorley.com>
2026-02-27 15:27:19 -0500

    Update appVersion to 'v0.12.0-main-022726'

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-export let appVersion = 'v0.12.0';
+export let appVersion = 'v0.12.0-main-022726';
 export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.12.0';
 export let appTitle = 'AdventureLog';
 export let copyrightYear = '2023-2026';
