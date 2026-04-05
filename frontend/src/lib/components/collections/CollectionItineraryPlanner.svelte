5c659c32e730b225cedc88304ffeb4e53e456ea0
Sean Morley <git@seanmorley.com>
2026-04-04 20:48:50 -0400

    fix: update appVersion to v0.12.0-main-040426 and refactor button elements to improve accessibility in CollectionCard and CollectionItineraryPlanner components

--- frontend/src/lib/components/collections/CollectionItineraryPlanner.svelte
+++ frontend/src/lib/components/collections/CollectionItineraryPlanner.svelte
@@ -1957,15 +1957,16 @@
 
 							{#if canModify}
 								<div class="dropdown z-30">
-									<button
-										type="button"
+									<div
+										tabindex="0"
+										role="button"
 										class="btn btn-square btn-sm btn-outline p-1"
 										aria-haspopup="menu"
 										aria-expanded="false"
 										title={$t('adventures.add')}
 									>
 										<Plus class="w-5 h-5" />
-									</button>
+									</div>
 									<ul
 										tabindex="-1"
 										class="dropdown-content menu p-2 shadow bg-base-300 rounded-box w-56"
