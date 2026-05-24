7a6cbaddfb6fa5d79ff1e2066ee6ffb22b6d984a
Sean Morley <git@seanmorley.com>
2026-05-24 09:14:50 -0400

    fix(config): update appVersion to remove suffix and align with release v0.12.1

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-export let appVersion = 'v0.12.1-main-052626';
+export let appVersion = 'v0.12.1';
 export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.12.1';
 export let appTitle = 'AdventureLog';
 export let copyrightYear = '2023-2026';
