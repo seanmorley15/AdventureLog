18f8bc85b53c6a0a7f510630fc4151374f23d9cd
zv0n <tom.zvon@gmail.com>
2026-06-03 20:32:37 +0200

    backend - allow rootless execution

--- backend/server/main/settings.py
+++ backend/server/main/settings.py
@@ -368,7 +368,7 @@ LOGGING = {
         },
         'file': {
             'class': 'logging.FileHandler',
-            'filename': 'scheduler.log',
+            'filename': getenv('LOG_FILE', 'scheduler.log'),
         },
     },
     'root': {
