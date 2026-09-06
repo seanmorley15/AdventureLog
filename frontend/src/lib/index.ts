4330febb433d4a54c07ef442861d5d796cab3098
Sylvain Bataille <sylvain.bataille@nrb.be>
2026-09-06 18:11:47 +0200

    add carto basemap api key to frontend (#1277)
    Co-authored-by: Sean Morley <git@seanmorley.com>

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,3 +1,4 @@
+import { env } from '$env/dynamic/public';
 import inspirationalQuotes from './json/quotes.json';
 import randomBackgrounds from './json/backgrounds.json';
 
@@ -687,7 +688,9 @@ export function getBasemapUrl(type = 'default'): any {
 				sources: {
 					'raster-tiles': {
 						type: 'raster',
-						tiles: ['https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'],
+						tiles: [
+							cartoUrl('https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png')
+						],
 						tileSize: 256,
 						attribution: '© OpenStreetMap contributors, © CARTO'
 					},
@@ -897,32 +900,32 @@ export function getBasemapUrl(type = 'default'): any {
 
 		case 'carto-light':
 			return getXYZStyle(
-				'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+				cartoUrl('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'),
 				'© OpenStreetMap contributors, © CartoDB'
 			);
 
 		case 'carto-dark':
 			return getXYZStyle(
-				'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+				cartoUrl('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'),
 				'© OpenStreetMap contributors, © CartoDB'
 			);
 
 		case 'carto-positron':
 			return getXYZStyle(
-				'https://basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
+				cartoUrl('https://basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png'),
 				'© OpenStreetMap contributors, © CARTO'
 			);
 
 		case 'carto-positron-labels':
 			return getXYZStyle(
-				'https://basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png',
+				cartoUrl('https://basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png'),
 				'© OpenStreetMap contributors, © CARTO',
-				'https://basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png'
+				cartoUrl('https://basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png')
 			);
 
 		case 'carto-voyager':
 			return getXYZStyle(
-				'https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+				cartoUrl('https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'),
 				'© OpenStreetMap contributors, © CARTO'
 			);
 
@@ -977,9 +980,14 @@ export function getBasemapUrl(type = 'default'): any {
 
 		default:
 			return getIsDarkMode()
-				? 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
-				: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json';
+				? cartoUrl('https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json')
+				: cartoUrl('https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json');
 	}
+}
+
+function cartoUrl(url: string): string {
+	const key = env.PUBLIC_CARTO_BASEMAP_API_KEY;
+	return key ? `${url}?key=${encodeURIComponent(key)}` : url;
 }
 
 // Helper function for XYZ tile styles (updated to handle arrays and remove {s} placeholders)
